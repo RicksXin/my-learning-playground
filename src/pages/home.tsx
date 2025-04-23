678a61b382eaa1d3180299b5d70c7968371a99e1
Sean <770899447@qq.com>
2025-04-23 16:23:11 +0800

    Squashed 'projects/basic/visualization/' content from commit e728e3a
    git-subtree-dir: projects/basic/visualization
    git-subtree-split: e728e3acc0c16f197792f58b0f4982caadeb4ca6

---
+++ src/pages/home.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import image from '../images/header.png';
+import './home.scss';
+import { Chart1 } from './Components/charts/charts-1';
+import { Chart2 } from './Components/charts/charts-2';
+import { Chart3 } from './Components/charts/charts-3';
+import { Chart4 } from './Components/charts/charts-4';
+import { Chart5 } from './Components/charts/charts-5';
+import { Chart6 } from './Components/charts/charts-6';
+import { Chart7 } from './Components/charts/charts-7';
+import { Chart8 } from './Components/charts/charts-8';
+import { Chart9 } from './Components/charts/charts-9';
+
+export const Home = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="home">
+      <header style={{ backgroundImage: `url(${image})` }}></header>
+      <main>
+        <section className="section1">
+          <Chart1 />
+          <Chart2 />
+        </section>
+        <section className="section2">
+          <Chart3 />
+          <Chart4 />
+        </section>
+        <section className="bordered section3">
+          <Chart5 />
+        </section>
+        <section className="section4">
+          <Chart6 />
+          <div className="bordered year-old-range">
+            <h2>犯罪人员年龄段分布</h2>
+            <div className="charts">
+              <Chart7 />
+              <Chart8 />
+              <Chart9 />
+            </div>
+          </div>
+        </section>
+        <section className="section5">
+          <div className="borderd sec"></div>
+        </section>
+      </main>
+      <footer> &copy; Sean 2020-{year} </footer>
+    </div>
+  );
+};
